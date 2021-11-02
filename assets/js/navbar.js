38f8603f237aebfcc696000d4f0f6ffb467a5a22
Salih Saygi <salih.saygie@gmail.com>
2021-11-01 23:26:10 -0700

    updated navbar script for full version (deprecations)

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,14 +1,9 @@
-$(document).ready(function()
-{
-	$(window).scroll(function()
-	{
-		if ($(window).scrollTop() > $(window).height()/3)
-		{
-			$("#navbar").addClass("navbar-sticky");
-		}
-		else
-		{
-			$("#navbar").removeClass("navbar-sticky");
-		}
-	});
+$(function () {
+  $(window).on('scroll')(function () {
+    if ($(window).scrollTop() > $(window).height() / 3) {
+      $('#navbar').addClass('navbar-sticky');
+    } else {
+      $('#navbar').removeClass('navbar-sticky');
+    }
+  });
 });
