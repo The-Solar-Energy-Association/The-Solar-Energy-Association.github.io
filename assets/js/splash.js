6eca9a9ecd26569f4da34ea02e50271e73c22728
Shawn Duong <shawnduong@protonmail.com>
2021-11-02 18:09:32 -0700

    Restyled JS.

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -1,10 +1,11 @@
-$(function () {
-  $('#learn-more').on('click', function () {
-    $('html,body').animate(
-      {
-        scrollTop: $('#about').offset().top,
-      },
-      'slow'
-    );
-  });
+$(function ()
+{
+	$("#learn-more").on("click", function ()
+	{
+		$("html, body").animate(
+		{
+			scrollTop: $("#about").offset().top,
+		},
+		"slow");
+	});
 });
